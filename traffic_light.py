ca87af4dc2d2feb04b0acddfadeb015ab07500b4
Adenuga Banjoko <banjokoa@uni.coventry.ac.uk>
2015-01-09 13:18:40 +0000

    Update traffic_light.py

--- traffic_light.py
+++ traffic_light.py
@@ -1,6 +1,8 @@
-from Pygame import*
+import pygame # import pygame api 
+from pygame.locals import*
 import random
-window = Tk()
+
+window = pygame()
 arena = Canvas(window, width = 500, height = 500, bg = 'white')
 arena.pack()
 key = Canvas(window, width = 500, height = 170, bg = 'grey')
