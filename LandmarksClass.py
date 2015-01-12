586708d7a751db8d9518ddab0e218f921cce755c
femibak96 <femz300@gmail.com>
2015-01-12 16:33:34 +0000

    Create LandmarksClass.py

---
+++ LandmarksClass.py
@@ -0,0 +1,10 @@
+class Landmarks:
+	def __init__ (self):
+		self.name = ""
+		self.treasure = False
+		self.locationX = 0
+		self.locationY = 0
+		self.image = ""
+		self.infotext = ""
+	def noTreasure (self):
+                self.treasure = False
