8f39e8d4771b3b036a372b06f0ab3a37dbf58634
samathy <milktheelephant@gmail.com>
2015-01-13 15:41:53 +0000

    Added movement to robot class

--- robot.py
+++ robot.py
@@ -1,28 +1,38 @@
 class robot:
-    def __init__(self):
-        self.locationX = ""
-        self.locationY = ""
+    
+
+    locationX = 0 #The location location of the robot
+    locationY = 0
+
+    name = ""   #The name of the Robot
+
+    image = None  #the image which represents the image. (Could make an image object?)
+
+    points = 0    #The amount of points this robot has
+    goal = ""   #???
+
+    
+
+
+
+    def __init__(self): #Constructor
+
+        self.locationX = 0 
+        self.locationY = 0 
         self.name = ""
-        self.image = ""
-        self.points = 0
+        self.image = open("robot.png", 'r') #Open the robot image
+
+        self.points = 0 
         self.goal = ""
 
-    def moveUp(self):
-        print "up"
-        #insert movement code.
-
+    def moveUp(self): #Change the location of the robot to make it move up
+        self.locationY -= 1
     def moveDown(self):
-        print "down"
-        #insert movement code.
-
+        self.locationY += !
     def moveLeft(self):
-        print "left"
-        #insert movement code.
-
+        self.locationY +=20
     def moveRight(self):
-        print "right"
-        #insert movement code.
-
+        self.locationX += 20
     def plotPath(self):
         print "find path"
         #insert DANNYS code.
