0e87b4c8bf081607896c362e2aea89bca85fe170
Dan Beglin <dbeglinuk@gmail.com>
2015-01-13 11:43:18 +0000

    Removed Plagarised Code
    Added legal code which is also OOP compliant.

--- robot.py
+++ robot.py
@@ -1,4 +1,41 @@
-#
-#   This file should contain all the elements of the Robot class and its methods and assotiated data.
-#
+class robot:
+    def __init__(self):
+        self.locationX = ""
+        self.locationY = ""
+        self.name = ""
+        self.image = ""
+        self.points = 0
+        self.goal = ""
 
+    def moveUp(self):
+        print "up"
+        #insert movement code.
+
+    def moveDown(self):
+        print "down"
+        #insert movement code.
+
+    def moveLeft(self):
+        print "left"
+        #insert movement code.
+
+    def moveRight(self):
+        print "right"
+        #insert movement code.
+
+    def plotPath(self):
+        print "find path"
+        #insert DANNYS code.
+
+    def checkLight(self):
+        print "check light"
+        #insert code to check if there is a traffic light near
+
+    def passbyLandmark(self):
+        print "passby text display"
+        #insert PHILS code for text display
+
+    def waitForLights(self):
+        time.sleep(2)
+        #make robot wait two seconds.
+        
