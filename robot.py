f73ff6688860efd96c6ee0c2bbdb31b46fbb3341
philhabell <philhabell@gmail.com>
2015-01-20 14:52:17 +0000

    fixed stuff

--- robot.py
+++ robot.py
@@ -85,81 +85,81 @@ class robot:
         self.treasureFound = 0
         
         if arena.ret_element_val(self.locationX, self.locationY) = 6:   #PHIL XXX XXX This is the line i've corrected, its atleast syntactically correct and should be able to interact with the Arena class. 
-            landmarkVar = "Pyramid"                                     #But obvously we can't test it r/n
-            self.landInfo = wikipedia.summary("Pyramids", sentences = 2)
+            self.landmarkVar = "Pyramid"                                     #But obvously we can't test it r/n
+            self.landInfo = wikipedia.summary(self.ladmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[,] = 7:
-            landmarkVar "Big Ben"
-            self.landInfo = wikipedia.summary("Big Ben", sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 7:
+            self.landmarkVar "Big Ben"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
 
-        elif self.arena[self.locationX,self.locationY] = 8:
-            landmarkVar = "Taj Mahal"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 8:
+            self.landmarkVar = "Taj Mahal"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 9:
-            landmarkVar = "Cloud Gate"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 9:
+            self.landmarkVar = "Cloud Gate"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 10:
-            landmarkVar = "Arc De Triomphe"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 10:
+            self.landmarkVar = "Arc De Triomphe"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 11:
+        elif self.ret_element_val(self.locationX, self.locationY) = 11:
             ladmarkVar = "Todaji Temple"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 12:
-            landmarkVar = "The Gherkin"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 12:
+            self.landmarkVar = "The Gherkin"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 13:
-            landmarkVar = "The Shard"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 13:
+            self.landmarkVar = "The Shard"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 14:
-            landmarkVar = "The Statue Of Liberty"
-            self.landInfo = wikipedia.summ ary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 14:
+            self.landmarkVar = "The Statue Of Liberty"
+            self.landInfo = wikipedia.summ ary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 15:
-            landmarkVar = "Maracana Stadium"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 15:
+            self.landmarkVar = "Maracana Stadium"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 16:
-            landmarkVar = "Ayers Rock"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 16:
+            self.landmarkVar = "Ayers Rock"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 17:
-            landmarkVar = "Stone Henge"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 17:
+            self.landmarkVar = "Stone Henge"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
-        elif self.arena[self.locationX,self.locationY] = 18:
-            landmarkVar = "City Hall"
-            self.landInfo = wikipedia.summary(landmarkVar, sentences = 2)
+        elif self.ret_element_val(self.locationX, self.locationY) = 18:
+            self.landmarkVar = "City Hall"
+            self.landInfo = wikipedia.summary(self.landmarkVar, sentences = 2)
             treasureCheck(treasureLandmark, arena, treasureFound)
 
 
