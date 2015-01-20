c504e2a8ce82b8ca606309cc79203c7ffc3d3541
samathy <milktheelephant@gmail.com>
2015-01-20 15:27:40 +0000

    We're getting there....(Stashingto add functionality)

--- main.py
+++ main.py
@@ -19,19 +19,22 @@ def insertTrafficLights(ar, lights, num_lights):   #Takes an instance of arena a
 
     print "Using width:"+str(width)
     print "Using height:"+str(height)
+    
+    placed = False
 
     for i in range(0, num_lights):
+        while placed != True:
 
-        rand_row = random.randint(0,height-1) #Pick a random row to spawn a trafficlight
-
-        for x in range(0, width):
-            print height, x
-            print ar.ret_element_value(height-1, x)
-            if ar.ret_element_value(height-1, x) == 1:
-                lights[x] = trafficLights(rand_row, x) #Add a new traffic light.
-                ar.put(rand_row, x)
-                picked.append(rand_row)             #append the thing so we know not to put two lights on the same row
-    
+           rand_row = random.randint(0,height-1) #Pick a random row to spawn a trafficlight
+   
+           for x in range(0, width):
+               print rand_row, x
+               print ar.ret_element_value(rand_row, x)  #Why does this always return 0???? XXX
+               if ar.ret_element_value(rand_row, x) == 1 or ar.ret_element_value(rand_row, x) == 2:
+                   lights[x] = trafficLights(rand_row, x) #Add a new traffic light.
+                   ar.put(rand_row, x)
+                   picked.append(rand_row)             #append the thing so we know not to put two lights on the same row
+                   placed = True
 
 
 
@@ -49,6 +52,7 @@ def main():
     arena = gen_arena.arena(mapName+".png")
     arena.show_arena()
     insertTrafficLights(arena, lights, 3)
+    print arena.ret_element_value(0, 9) #Give row, col
     print lights
     arena.show_arena()
 
