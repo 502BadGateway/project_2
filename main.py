49f74e3c91a7e0c25b537da62f1f7b7bce52cb8e
Sam Barratt <milktheelephant@gmail.com>
2015-01-21 15:54:06 +0000

    Trafficlights work better

--- main.py
+++ main.py
@@ -13,24 +13,27 @@ import random
 
 
 def insertTrafficLights(ar, lights, num_lights):   #Takes an instance of arena and places the traffic light elements into that arena
-    width = ar.ret_size()[0]
-    height = ar.ret_size()[1]
-    picked = [3]
+    width = ar.ret_size()[0]-1        #Get the width and heights of the array
+    height = ar.ret_size()[1]-1
+    picked = [3]                    #Store how many we have picked TODO Do i really need to use this, might not be nessesary
 
-    print "Using width:"+str(width)
+    print "Using width:"+str(width) #dbg
     print "Using height:"+str(height)
     
-    placed = False
+    placed = False                  #Store if we've placed anything
+    x = 0
 
     for i in range(0, num_lights):
-        while placed != True:
+        while placed != True and x < width:
 
-           rand_row = random.randint(0,height-1) #Pick a random row to spawn a trafficlight
+           rand_row = random.randint(0,height) #Pick a random row to spawn a trafficlight
    
-           for x in range(0, width):
+           #for x in range(0, width):    #look at every element in the witdh
+           while x <= width-1 or placed != True:
                print rand_row, x
-               print ar.ret_element_value(rand_row, x)  #Why does this always return 0???? XXX
-               if ar.ret_element_value(rand_row, x) == 1 or ar.ret_element_value(rand_row, x) == 2:
+               print ar.ret_size()
+               #print ar.ret_element_value(rand_row, x)  #Why does this always return 0???? XXX
+               if ar.ret_element_value(rand_row, x-1) == 1 or ar.ret_element_value(rand_row, x-1) == 2:
                    lights.append(traffic_light.trafficLights(rand_row, x)) #Add a new traffic light.
                    while ar.ret_element_value(rand_row, x) == 1 and x < width:
                        x = x+1
@@ -40,6 +43,8 @@ def insertTrafficLights(ar, lights, num_lights):   #Takes an instance of arena a
                        ar.put(rand_row, x+1, 3)
                        picked.append(rand_row)             #append the thing so we know not to put two lights on the same row
                        placed = True
+               x += 1
+           placed = False     #Make sure we reset the if we place anything
 
 
 
