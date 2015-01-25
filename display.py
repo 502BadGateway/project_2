925ff8d1e618c3e9bb3815f44435e044db83b041
philhabell <philhabell@gmail.com>
2015-01-25 13:17:01 +0000

    moved methods that display to display class

--- display.py
+++ display.py
@@ -46,5 +46,84 @@ class display:      #Class which handles all the display functionality.
 
         return
 
+    def CreateText(self, arena, treasureLandmark, landInfo):
+
+        print "Text Outputting..."
+
+        sys.setdefaultcoding('utf-8') #converts to UNICODE
+        self.textFont = pygame.font.Font("Comic Sans MS",15) #sets font and text size
+        self.landInfoText = self.textFont.render(landInfo,1,(10,10,10)) #loads the text to be displayed to the screen (need to work out how to word wrap)
+        screen.blit(self.landInfoText,(0,0)) #blits the screen and sets the poistion I think (not sure becuase I cant test it)
+        pygame.display.flip() #refreshes display
+
+
+    def waitForLights(self):
+        time.sleep(2)
+        #make robot wait two seconds.
+    
+    def RobotPoints(self,robotScore,positionVar): #this should display pauls points but im not sure if this needs to be done for both robots
+
+        self.scoreFont = pygame.font.Font("Comic Sans MS",30)
+        self.scoreText = self.scoreFont.render(self.robotScore,1,(10,10,10))
+        self.textPos = pygame.Rect(positionVar) #positionVar needs to be given to this method, it should be in the format "600,10,0,0" and "600,30,0,0"
+        background.blit(scoreText, textPos)
+        screen.blit(background, (0,0))
+        pygame.display.flip()
+
+    def render_textrect(landInfo, font, rect, text_color, background_color, justification=0): #this code will wordwrap text for you IT IS NOT MINE it is from "http://www.pygame.org/pcr/text_rect/index.php"
+
+        import pygame
+        
+        final_lines = []
+
+        requested_lines = landInfo.splitlines()
+
+        # Create a series of lines that will fit on the provided
+        # rectangle.
+
+        for requested_line in requested_lines:
+            if font.size(requested_line)[0] > rect.width:
+                words = requested_line.split(' ')
+                # if any of our words are too long to fit, return.
+                for word in words:
+                    if font.size(word)[0] >= rect.width:
+                        raise TextRectException, "The word " + word + " is too long to fit in the rect passed."
+                # Start a new line
+                accumulated_line = ""
+                for word in words:
+                    test_line = accumulated_line + word + " "
+                    # Build the line while the words fit.    
+                    if font.size(test_line)[0] < rect.width:
+                        accumulated_line = test_line 
+                    else: 
+                        final_lines.append(accumulated_line) 
+                        accumulated_line = word + " " 
+                final_lines.append(accumulated_line)
+            else: 
+                final_lines.append(requested_line) 
+
+        # Let's try to write the text out on the surface.
+
+        surface = pygame.Surface(rect.size) 
+        surface.fill(background_color) 
+
+        accumulated_height = 0 
+        for line in final_lines: 
+            if accumulated_height + font.size(line)[1] >= rect.height:
+                raise TextRectException, "Once word-wrapped, the text string was too tall to fit in the rect."
+            if line != "":
+                tempsurface = font.render(line, 1, text_color)
+                if justification == 0:
+                    surface.blit(tempsurface, (0, accumulated_height))
+                elif justification == 1:
+                    surface.blit(tempsurface, ((rect.width - tempsurface.get_width()) / 2, accumulated_height))
+                elif justification == 2:
+                    surface.blit(tempsurface, (rect.width - tempsurface.get_width(), accumulated_height))
+                else:
+                    raise TextRectException, "Invalid justification argument: " + str(justification)
+            accumulated_height += font.size(line)[1]
+
+        return surface
+
 
 #dis = display("map1.png")  #dbg
