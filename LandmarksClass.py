0eb54f118825af709141efcf389d76277ead0487
Sam Barratt <milktheelephant@gmail.com>
2015-01-25 13:52:34 +0000

    Added constructor arguments for landmark class

--- LandmarksClass.py
+++ LandmarksClass.py
@@ -1,10 +1,10 @@
 class Landmarks:
-	def __init__ (self):
-		self.name = ""
-		self.treasure = False
-		self.locationX = 0
-		self.locationY = 0
-		self.image = ""
-		self.infotext = ""
+	def __init__ (self, name, treasure, x, y, image, infotext, treasure=False):
+		self.name = name 
+		self.treasure = treasure 
+		self.locationX = x
+		self.locationY = y
+		self.image = image 
+		self.infotext = infotext 
 	def noTreasure (self):
                 self.treasure = False
