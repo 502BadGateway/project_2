01efe4fdd3aaa98d3a951902113e13919cca2897
femibak96 <femz300@gmail.com>
2015-01-28 10:58:49 +0000

    Update LandmarksClass.py

--- LandmarksClass.py
+++ LandmarksClass.py
@@ -1,10 +1,10 @@
 class Landmarks: #this class is what spawns the image
 	def __init__ (self, x, y, treasure, name, image):
 		self.name = name 
-		self.treasure = treasure #tells the robto there isnt any treasreu
+		self.treasure = treasure #tells the robot there isn't any treasre
 		self.locationX = x #the x coodrinate
-		self.locationY = y #teh y coordinate
+		self.locationY = y #the y coordinate
 		self.image = "ASSETS/"+str(image)  # loads the image of the landmark
 		self.infotext = None #loads the text from the wikipedia page
-	def noTreasure (self): #desicdes if there is treasure
+	def noTreasure (self): #decides if there is treasure
                 self.treasure = False
