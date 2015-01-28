329bbc81a85efecb35192cf8c96e01d991c2bec3
femibak96 <femz300@gmail.com>
2015-01-28 11:00:02 +0000

    Update Sound.py

--- Sound.py
+++ Sound.py
@@ -5,10 +5,10 @@ pygame.init() #initiates pygame
 white = (255, 64, 64) #this is the colour, to be used later
 w = 640 #the width of the dispaly varibale
 h = 480 #the height of the display variable
-screen = pygame.display.set_mode((w, h)) #creates the the dispay
+screen = pygame.display.set_mode((w, h)) #creates the the display
 screen.fill((white)) 
 
-song = pygame.mixer.Sound('backgroundMusic.ogg') #creates an instants of the music
+song = pygame.mixer.Sound('backgroundMusic.ogg') #creates an instant of the music
 song.play() plays the music
 while 1:
     for x in range(0,10000000):
